9acac6998e0a2eab6dce393395d2312302c2aab1
AlphCSR <cesarleon908@gmail.com>
2025-05-22 11:27:41 -0400

    Add Docs

--- UsersMS.Infrastructure/Migrations/20250522145851_InitialCreate.cs
+++ UsersMS.Infrastructure/Migrations/20250522145851_InitialCreate.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace UsersMS.Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialGuidUserMigration : Migration
+    public partial class InitialCreate : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
